ddabe0659f56a10f8fc7230311ecc9b09da81025
IshanRai9 <raiishan0911@gmail.com>
2025-07-17 01:01:37 +0530

    import

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from analyze_text import summaries
+from analyze_text import summaries 
 
 st.title("Real-Time Disaster Alerts")
 st.write("Fetching latest disaster-related data...")
